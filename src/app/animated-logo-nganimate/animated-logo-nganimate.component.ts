47e053b0b9bed7092ec57b9871dc7d6930c4ad72
engalar <liuwengao@foxmail.com>
2017-08-27 13:44:12 +0800

    test mouse event animate

--- src/app/animated-logo-nganimate/animated-logo-nganimate.component.ts
+++ src/app/animated-logo-nganimate/animated-logo-nganimate.component.ts
@@ -8,6 +8,17 @@ import { Component, animate, keyframes, state, style, transition, trigger } from
     trigger('aState', [
       state('small', style({transform: 'scale(1)'})),
       state('large', style({transform: 'scale(4.2)'})),
+      state('over', style({fill: 'red'})),
+      transition('* => over', animate('5s ease', keyframes([
+        style({fill: 'blue', offset: 0}),
+        style({fill: 'green', offset: 0.3}),
+        style({fill: 'red', offset: 0.6}),
+      ]))),
+      transition('over => *', animate('5s ease', keyframes([
+        style({fill: 'red', offset: 0}),
+        style({fill: 'green', offset: 0.3}),
+        style({fill: 'white', offset: 0.6}),
+      ]))),
       transition('small => large', animate('1s ease', keyframes([
         style({transform: 'scale(1)', offset: 0}),
         style({transform: 'scale(0.7) rotate(15deg)', offset: 0.15}),
@@ -28,5 +39,13 @@ export class AnimatedLogoNganimateComponent {
 
   toggleAState() {
     this.aState = this.aState === 'small' ? 'large' : 'small';
+  }
+  mouseouthandle() {
+    this.aState = 'small';
+    console.log('out');
+  }
+  mouseenterhandle() {
+    this.aState = 'over';
+    console.log('enter');
   }
 }
